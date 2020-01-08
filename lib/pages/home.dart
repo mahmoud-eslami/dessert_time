23cfbd96268c3cb08d65ce04cbcf42dced0b0ae7
Mahmoud Eslami <eslamimahmoud98@gmail.com>
2020-01-08 15:51:42 +0330

    Add curvedNavigationBar to main Screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,6 +10,16 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: Colors.orange,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.compare_arrows, size: 30),
+        ],
+      ),
       body: Column(
         children: <Widget>[
           SizedBox(height: 25,),

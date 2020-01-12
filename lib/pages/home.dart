d5d4fbfff1f16b05ef8fbe20eb06833ec40c0de1
Mahmoud Eslami <eslamimahmoud98@gmail.com>
2020-01-12 21:13:29 +0330

    add ShopItem

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,41 +21,110 @@ class _HomeState extends State<Home> {
           backgroundColor: Colors.white,
           bottomNavigationBar: CustomBottomNavigation(),
           body: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Text(
-                      'Discover dessert\naround the world',
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Discover dessert\naround the world',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Image.asset(
+                        'assets/images/cookie.png',
+                        height: 50,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  DessertItem(
+                    image: 'assets/images/cookie.png',
+                    country: 'iran',
+                    name: 'cake',
+                    description: 'desasfqweasf',
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Nearby CoffeShop',
                       style: TextStyle(
-                        fontSize: 30,
+                        fontSize: 15,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Image.asset(
-                      'assets/images/cookie.png',
-                      height: 50,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                DessertItem(
-                  image: 'assets/images/cookie.png',
-                  country: 'iran',
-                  name: 'cake',
-                  description: 'desasfqweasf',
-                ),
-              ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ShopItem(
+                    image: 'assets/images/cafe.png',
+                    shopName: 'bazzar',
+                    description: 'niceeee',
+                    openTime: '9 - 13',
+                  )
+                ],
+              ),
             ),
           ),
         );
       },
+    );
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  final String image;
+  final String shopName;
+  final String description;
+  final String openTime;
+
+  const ShopItem(
+      {Key key, this.image, this.shopName, this.description, this.openTime})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.brown.withOpacity(.6),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: <Widget>[
+            Image.asset(
+              image,
+              height: 70,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              children: <Widget>[
+                Text(shopName),
+                Text(description),
+                Text(openTime),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
@@ -73,14 +142,14 @@ class DessertItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
+      height: 230,
       width: 180,
       decoration: BoxDecoration(
         color: Colors.brown.withOpacity(.8),
         borderRadius: BorderRadius.circular(30),
       ),
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
+        padding: EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -88,6 +157,9 @@ class DessertItem extends StatelessWidget {
             Image.asset(
               image,
               height: 90,
+            ),
+            SizedBox(
+              height: 15,
             ),
             Text(
               country,
@@ -100,7 +172,7 @@ class DessertItem extends StatelessWidget {
               name,
               style: TextStyle(
                   color: Colors.white,
-                  fontSize: 20,
+                  fontSize: 25,
                   fontWeight: FontWeight.w900),
             ),
             Text(

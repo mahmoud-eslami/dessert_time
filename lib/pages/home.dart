228b9128c6c6f01c4dd24691168a93a6c7e27f0e
Mahmoud Eslami <eslamimahmoud98@gmail.com>
2020-01-12 20:10:00 +0330

    add dessert item to heme

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,33 +20,103 @@ class _HomeState extends State<Home> {
         return Scaffold(
           backgroundColor: Colors.white,
           bottomNavigationBar: CustomBottomNavigation(),
-          body: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    'Discover dessert\naround the world',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      'Discover dessert\naround the world',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.black,
-                  ),
-                ],
-              )
-            ],
+                    Image.asset(
+                      'assets/images/cookie.png',
+                      height: 50,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                DessertItem(
+                  image: 'assets/images/cookie.png',
+                  country: 'iran',
+                  name: 'cake',
+                  description: 'desasfqweasf',
+                ),
+              ],
+            ),
           ),
         );
       },
     );
   }
 }
+
+class DessertItem extends StatelessWidget {
+  final String image;
+  final String name;
+  final String description;
+  final String country;
+
+  const DessertItem(
+      {Key key, this.image, this.name, this.description, this.country})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 180,
+      decoration: BoxDecoration(
+        color: Colors.brown.withOpacity(.8),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset(
+              image,
+              height: 90,
+            ),
+            Text(
+              country,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500),
+            ),
+            Text(
+              name,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900),
+            ),
+            Text(
+              description,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.w300),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class CustomBottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -54,9 +124,7 @@ class CustomBottomNavigation extends StatelessWidget {
       showSelectedLabels: false,
       showUnselectedLabels: false,
       elevation: 110,
-      iconSize: 30,
-      fixedColor: Colors.blue,
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.brown,
       type: BottomNavigationBarType.fixed,
       items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(
@@ -66,7 +134,7 @@ class CustomBottomNavigation extends StatelessWidget {
           ),
           icon: Icon(
             Icons.home,
-            color: Colors.black,
+            color: Colors.white,
           ),
         ),
         BottomNavigationBarItem(
@@ -76,7 +144,7 @@ class CustomBottomNavigation extends StatelessWidget {
           ),
           icon: Icon(
             Icons.search,
-            color: Colors.black,
+            color: Colors.white,
           ),
         ),
         BottomNavigationBarItem(
@@ -85,8 +153,8 @@ class CustomBottomNavigation extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           ),
           icon: Icon(
-            Icons.insert_drive_file,
-            color: Colors.black,
+            Icons.folder_open,
+            color: Colors.white,
           ),
         ),
         BottomNavigationBarItem(
@@ -95,12 +163,11 @@ class CustomBottomNavigation extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           ),
           icon: Icon(
-            Icons.account_box,
-            color: Colors.black,
+            Icons.account_circle,
+            color: Colors.white,
           ),
         ),
       ],
     );
   }
 }
-

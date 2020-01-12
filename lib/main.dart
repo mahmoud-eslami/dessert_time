beaef0474321f11d957f48af4fd550f4d90fd257
Mahmoud Eslami <eslamimahmoud98@gmail.com>
2020-01-12 16:40:13 +0330

    defind bottom navigation in another method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dessert_time/bloc/dessert_bloc.dart';
 import 'package:dessert_time/bloc/dessert_event.dart';
+import 'package:dessert_time/pages/home.dart';
 import 'package:dessert_time/pages/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           fontFamily: 'Baskervville',
         ),
-        home: SplashScreen(),
+        home: Home(),
       ),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
+import 'package:dessert_time/bloc/bloc.dart';
+import 'package:dessert_time/pages/splash.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,59 +12,81 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: Colors.blue,
-        items: <BottomNavigationBarItem>[
-//        Icon(Icons.account_circle),
-          BottomNavigationBarItem(
-            title: Text('',style: TextStyle(color: Colors.white),),
-            icon: Icon(
-              Icons.home,
-              color: Colors.white,
-            ),
-          ),
-          BottomNavigationBarItem(
-            title: Text('',style: TextStyle(color: Colors.white),),
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-          ),
-          BottomNavigationBarItem(
-            title: Text('',style: TextStyle(color: Colors.white),),
-            icon: Icon(
-              Icons.insert_drive_file,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 25,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return BlocBuilder<DessertBloc, DessertState>(
+      builder: (context, state) {
+        if (state is SplashState) {
+          return SplashScreen();
+        } else if (state is InitialDessertState) {}
+        return Scaffold(
+          backgroundColor: Colors.white,
+          bottomNavigationBar: CustomBottomNavigation(),
+          body: Column(
             children: <Widget>[
-              Text(
-                'Discover dessert\naround the world',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+              SizedBox(
+                height: 25,
               ),
-              CircleAvatar(
-                backgroundColor: Colors.black,
-              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'Discover dessert\naround the world',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.black,
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      ),
+          ),
+        );
+      },
     );
   }
 }
+class CustomBottomNavigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      backgroundColor: Colors.brown,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          title: Text(
+            '',
+            style: TextStyle(color: Colors.white),
+          ),
+          icon: Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+        ),
+        BottomNavigationBarItem(
+          title: Text(
+            '',
+            style: TextStyle(color: Colors.white),
+          ),
+          icon: Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+        ),
+        BottomNavigationBarItem(
+          title: Text(
+            '',
+            style: TextStyle(color: Colors.white),
+          ),
+          icon: Icon(
+            Icons.insert_drive_file,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
+

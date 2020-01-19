a858b230d0efe70aa7d6b0b79986171b54a71e7e
Mahmoud Eslami <eslamimahmoud98@gmail.com>
2020-01-19 18:40:31 +0330

    add new font to application

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.brown,
-          fontFamily: 'Baskervville',
+          fontFamily: 'Roboto',
         ),
         home: Home(),
       ),

421c942681be30640330b4be8ad7fcbbd1789ab1
mahmoud-eslami <eslamimahmou98@gmail.com>
2020-02-27 15:44:49 +0330

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,93 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Dessert Time',
         theme: mainTheme,
-        home: Home(),
+        home: CustomWidget(),
+      ),
+    );
+  }
+}
+
+class CustomWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                IconButton(icon: Icon(Icons.arrow_back), onPressed: (){}),
+                Text('Back')
+              ],
+            ),
+            //**************************************************
+
+
+            //here add Image Widget
+            //**************************************************
+
+            SizedBox(
+              height: 30,
+            ),
+            Text("proccesed login for"),
+            Text(
+              "Login",
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Text('Username',style: TextStyle(fontSize: 15),),
+            TextField(
+              decoration: InputDecoration(
+                  suffixIcon: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Password',style: TextStyle(fontSize: 15),),
+            TextField(
+              decoration: InputDecoration(
+                  suffixIcon: Icon(
+                    Icons.keyboard,
+                    color: Colors.black,
+                  )),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: RaisedButton(
+                  onPressed: () {},
+                  splashColor: Colors.pink,
+                  color: Colors.pink,
+                  textColor: Colors.white,
+                  child: Text("Login"),
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: FlatButton(
+                onPressed: () {},
+                child: Text("FotgetPass"),
+                textColor: Colors.black,
+                color: Colors.transparent,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

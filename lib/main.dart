ab0453c9b3ee43c7ba21301e4f6cab4b005e11b0
mahmoud <eslamimahmoud98@gmail.com>
2020-03-03 11:12:43 +0330

    Add image App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dessert_time/pages/home.dart';
+import 'package:dessert_time/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bloc/dessert_bloc/dessert_bloc.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Dessert Time',
         theme: mainTheme,
-        home: CustomWidget(),
+        home: MainPage(),
       ),
     );
   }
